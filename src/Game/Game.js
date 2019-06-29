160b7dfbe01b1e6e9667fcf3bf3c872aaad57f40
RtxRulez <vafarulez@ya.ru>
2019-06-29 21:55:42 +0300

    цикл

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -16,48 +16,87 @@ class Game extends React.Component {
     let len = snake.length - 1;
     // console.log(snake[len])
     let newCord = [];
+
     if (to === 3) {
-      newCord = [snake[len][0], snake[len][1] + 1];
-
-      if (newCord[1] > 10) {
-        newCord = [snake[len][0], 0];
+      for (let i = 0; i < snake.length; i++) {
+        if (snake[i][1] >= 9) {
+          snake[i][1] = 0;
+        } else {
+          snake[i][1] = snake[i][1] + 1;
+        }
+      }
+    } else if (to === 9) {
+      for (let i = 0; i < snake.length; i++) {
+        if (snake[i][1] <= 0) {
+          snake[i][1] = 9;
+        } else {
+          snake[i][1] = snake[i][1] - 1;
+        }
+      }
+      
+    } else if (to === 6) {
+      for (let i = 0; i < snake.length; i++) {
+        if (snake[i][0] >= 9) {
+          snake[i][0] = 0;
+        } else {
+          snake[i][0] = snake[i][0] + 1;
+        }
+      }
+    } else if (to === 12) {
+      for (let i = 0; i < snake.length; i++) {
+        if (snake[i][0] <= 0) {
+          snake[i][0] = 9;
+        } else {
+          snake[i][0] = snake[i][0] - 1;
+        }
       }
     }
-    if (to == 6) {
-      newCord = [snake[len][0]+1, snake[len][1]];
+    console.log("snake", snake);
+    // } else if (to === 9) {
+    //   newCord = [snake[len][0], snake[len][1] - 1];
 
-      if (newCord[0] > 10) {
-        newCord = [0, snake[len][1]];
-      }
-    }
-    snake.push(newCord);
-    snake.shift(0);
+    //   if (newCord[1] < 0) {
+    //     newCord = [snake[len][0], 10];
+    //   }
+    // } else if (to == 6) {
+    //   newCord = [snake[len][0] + 1, snake[len][1]];
+
+    //   if (newCord[0] >= 10) {
+    //     newCord = [1, snake[len][0]];
+    //   }
+    // }
+    // snake.push(newCord);
     this.setState({
       snake: snake
     });
   }
 
-  onTop(e) {
-    this.walkSnake(3);
-  }
+  onTop = e => {
+    this.setState({
+      headAngle: 12
+    });
+  };
 
-  onBottom =(e) => {
+  onBottom = e => {
     this.setState({
       headAngle: 6
-    })
-  }
+    });
+  };
 
-  onLeft(e) {
-    this.walkSnake(6);
-  }
-
-  onRight = (e)  =>{
+  onRight = e => {
     this.setState({
       headAngle: 3
-    })
-  }
+    });
+  };
 
+  onLeft = e => {
+    this.setState({
+      headAngle: 9
+    });
+    // this.walkSnake(this.state.headAngle);
+  };
   componentDidMount() {
+    // this.walkSnake(this.state.headAngle);
     setInterval(() => {
       this.walkSnake(this.state.headAngle);
     }, 1000);
