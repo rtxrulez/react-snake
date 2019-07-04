06377b72a7ea445c134a051731f1428603c42916
RtxRulez <vafarulez@ya.ru>
2019-07-04 22:00:41 +0300

    билд

---
+++ build/precache-manifest.06caa8b535fc0a58c223c6b101c8704f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f980947574a048af07c57edeea3e0dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8114aea048d79679fff6",
+    "url": "/static/css/main.21e35af6.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "8114aea048d79679fff6",
+    "url": "/static/js/main.18fd645c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
